865dd863fb082f5f51fd8f26e414f87b7a1436f7
zilola08 <zmakhmonova@gmail.com>
2022-10-16 13:27:31 +0500

    carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || !sampleActivity) return false;
+  sampleActivityNumber = parseFloat(sampleActivity);
+  let modernActivity = 15; let k = Math.log(2) / 5730;
+  if (sampleActivityNumber > modernActivity || sampleActivityNumber == 0 || sampleActivityNumber < 0 || !sampleActivityNumber) {
+    return false;
+  } else {
+    let time = Math.ceil((Math.log(modernActivity / sampleActivityNumber)) / k);
+    return time;
+  }
 }
 
 module.exports = {
