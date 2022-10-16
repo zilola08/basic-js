a7c71fb4cb5ae7ca2e1655cb4c8ab424294ba6ec
zilola08 <zmakhmonova@gmail.com>
2022-10-16 15:16:31 +0500

    dns statst

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {},
+    dns = [],
+    one = [],
+    two = [],
+    three = [];
+
+  for (let i = 0; i < domains.length; i++) {
+    domains[i] = domains[i].split(".").reverse().join(".")
+    one.push(domains[i].slice(0, domains[i].indexOf(".")))
+    if (domains[i].indexOf(".") !== domains[i].lastIndexOf(".")) {
+      two.push(domains[i].slice(0, domains[i].lastIndexOf(".")))
+      three.push(domains[i].slice(0))
+    }
+    else { two.push(domains[i]) }
+  }
+  dns.push(one, two, three)
+  dns = dns.join(",").split(",")
+
+  dns = dns.filter(x => x !== "");
+
+  dns.forEach(
+    function (value, index, array) {
+      array[index] = '.' + value
+    }
+  )
+
+
+  dns.forEach(el => {
+    obj[el] = (obj[el] || 0) + 1;
+  });
+
+  return obj;
+
 }
 
 module.exports = {
